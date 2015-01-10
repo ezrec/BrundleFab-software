508326984c3de040171a91ded453a91c80dc778b
Jason S. McMullan <jason.mcmullan@gmail.com>
2015-01-10 13:24:55 -0500

    slicer: Explicitly call out feed rates
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

--- svg2brundlefab.sh
+++ svg2brundlefab.sh
@@ -2,10 +2,15 @@
 
 SVG_DIR="$1"
 
-Z="0"
+Z="0.35"
 Z_SLICE="0.50"
 X_MIN=-195
 X_MAX=180
+
+SPREAD_FEED=3000
+INK_FEED=4000
+DRY_FEED=5000
+POWDER_FEED=4500
 
 if [ -z "${SVG_DIR}" -o ! -d "${SVG_DIR}" ]; then
 	echo "Usage: $0 /path/to/svg/files"
@@ -18,7 +23,6 @@ G28 X0 Y0 Z0 E0 ; Home all axes
 G90 ; Absolute positioning
 M226 ; Wait for manual fill operation
 G92 ; Reset zeros after fill operation
-G1 F3000 ; Select feed rate (mm/minute)
 T1 S30000 ; Ink spray rate (dots/minute)
 T0 ; Select no tool
 EOF
@@ -29,15 +33,15 @@ for d in `ls ${SVG_DIR}/*.svg | sort`; do
 	cat <<EOF
 ; Perform layer operations
 T0 ; Select no tool
-G0 Y0 ; Reset ink head
-G0 X${X_MAX} ; Finish the layer deposition
-G0 Z${Z} ; Drop the build layer
+G0 Y0 ; Reset ink head position
+G1 X${X_MAX} F${SPREAD_FEED} ; Finish the layer deposition
+G0 Z${Z}; Drop the build layer
 T20 ; Select heat lamp tool
-G1 X0 Y0 ; Dry the layer
+G1 X0 ${DRY_FEED}; Dry the layer
 T0 ; Select no tool
-G1 X${X_MIN} Y0 ; Move to feed start
-G1 E${Z} ; Extrude a feed layer
-G1 X0 Y0 ; Move to print start
+G0 X${X_MIN} Y0 ; Move to feed start
+G1 E${Z} F${POWDER_FEED}; Extrude a feed layer
+G1 X0 Y0 F${SPREAD_FEED}; Move to print start
 ; Print as we feed powder
 EOF
 done
